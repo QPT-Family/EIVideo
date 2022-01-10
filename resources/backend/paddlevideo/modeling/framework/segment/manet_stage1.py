0c0e1e64ccbe7eac253d7786932b0ada547b56a1
AP-Kai <1433135401@qq.com>
2022-01-10 10:35:44 +0800

    Daily iter

--- resources/backend/paddlevideo/modeling/framework/segment/manet_stage1.py
+++ resources/backend/paddlevideo/modeling/framework/segment/manet_stage1.py
@@ -30,8 +30,6 @@ from ...builder import build_model
 from ...registry import SEGMENT
 from .base import BaseSegment
 
-from EIVideo.member import m
-
 # if cfg.MODEL.framework == "ManetSegment_Stage1":
 #     cfg_helper = {"knns": 1,
 #                   "is_save_image": True}
@@ -120,8 +118,8 @@ class ManetSegment_Stage1(BaseSegment):
         sequence = 'bike-packing'
         obj_nums = 1
         if sequence == 'drone':
-
-            images, _ = load_video(480)
+            video = 'data/1.mp4'
+            images = load_video(video, 480)
         elif sequence == 'bike-packing':
             images = get_images(sequence='bike-packing')
             obj_nums = 2
@@ -264,8 +262,7 @@ class ManetSegment_Stage1(BaseSegment):
                     )
                     print(paddle.unique(scribble_label))
                     final_masks = prev_label_storage
-                    m.inter_file_path = inter_file_path
-                    submit_masks(final_masks.numpy(), images)
+                    submit_masks(final_masks.numpy(), images, inter_file_path)
                     continue
 
                     ###inteaction segmentation head
@@ -464,8 +461,7 @@ class ManetSegment_Stage1(BaseSegment):
                 pred_masks_reverse.reverse()
                 pred_masks_reverse.extend(pred_masks)
                 final_masks = paddle.concat(pred_masks_reverse, 0)
-                m.inter_file_path = inter_file_path
-                submit_masks(final_masks.numpy(), images)
+                submit_masks(final_masks.numpy(), images, inter_file_path)
 
                 t_end = timeit.default_timer()
                 print('Total time for single interaction: ' +
