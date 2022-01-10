b30c12bcd68402c232a940c7c387dcf8de48aa12
liuchen21 <liuchen21@vaidu.com>
2022-01-10 11:34:56 +0800

    api of backend

--- resources/backend/main.py
+++ resources/backend/main.py
@@ -18,106 +18,26 @@ import numpy as np
 import paddle
 
 from resources.backend.paddlevideo.tasks import (test_model, train_dali, train_model,
-                               train_model_multigrid)
+                                                 train_model_multigrid)
 from resources.backend.paddlevideo.utils import get_config, get_dist_info
 
 
-def parse_args():
-    parser = argparse.ArgumentParser("PaddleVideo train script")
-    parser.add_argument('-c',
-                        '--config',
-                        type=str,
-                        default='configs/example.yaml',
-                        help='config file path')
-    parser.add_argument('-o',
-                        '--override',
-                        action='append',
-                        default=[],
-                        help='config options to be overridden')
-    parser.add_argument('--test',
-                        action='store_true',
-                        help='whether to test a model')
-    parser.add_argument('--train_dali',
-                        action='store_true',
-                        help='whether to use dali to speed up training')
-    parser.add_argument('--multigrid',
-                        action='store_true',
-                        help='whether to use multigrid training')
-    parser.add_argument('-w',
-                        '--weights',
-                        type=str,
-                        help='weights for finetuning or testing')
-    parser.add_argument('--fleet',
-                        action='store_true',
-                        help='whether to use fleet run distributed training')
-    parser.add_argument('--amp',
-                        action='store_true',
-                        help='whether to open amp training.')
-    parser.add_argument(
-        '--validate',
-        action='store_true',
-        help='whether to evaluate the checkpoint during training')
-    parser.add_argument(
-        '--seed',
-        type=int,
-        default=None,
-        help='fixed all random seeds when the program is running')
-    parser.add_argument(
-        '--max_iters',
-        type=int,
-        default=None,
-        help='max iterations when training(this argonly used in test_tipc)')
-    parser.add_argument(
-        '-p',
-        '--profiler_options',
-        type=str,
-        default=None,
-        help='The option of profiler, which should be in format '
-        '\"key1=value1;key2=value2;key3=value3\".')
-    parser.add_argument('--use_npu',
-                        type=bool,
-                        default=False,
-                        help='whether use npu.')
-
-    args = parser.parse_args()
-    return args
-
-
 def main():
-    args = parse_args()
-    cfg = get_config(args.config, overrides=args.override)
-
-    # set seed if specified
-    seed = args.seed
-    if seed is not None:
-        assert isinstance(
-            seed,
-            int), f"seed must be a integer when specified, but got {seed}"
-        paddle.seed(seed)
-        np.random.seed(seed)
-        random.seed(seed)
+    cfg = get_config('/home/lc/manet/save_step_80000/save_step_80000.pdparams', overrides=args.override)
 
     _, world_size = get_dist_info()
     parallel = world_size != 1
     if parallel:
         paddle.distributed.init_parallel_env()
 
-    if args.test:
-        test_model(cfg, weights=args.weights, parallel=parallel)
-    elif args.train_dali:
-        train_dali(cfg, weights=args.weights, parallel=parallel)
-    elif args.multigrid:
-        train_model_multigrid(cfg,
-                              world_size=world_size,
-                              validate=args.validate)
-    else:
-        train_model(cfg,
-                    weights=args.weights,
-                    parallel=parallel,
-                    validate=args.validate,
-                    use_fleet=args.fleet,
-                    amp=args.amp,
-                    profiler_options=args.profiler_options)
+    train_model(cfg,
+                weights='/home/lc/manet/save_step_80000/save_step_80000.pdparams',
+                # weights=args.weights,
+                parallel=parallel,
+                validate=args.validate,
+                use_fleet=args.fleet,
+                amp=args.amp,
+                profiler_options=args.profiler_options)
 
 
 if __name__ == '__main__':
