7bc0d9c8e15b2cde4972229507d4582f8619f9f8
liuchen21 <liuchen21@vaidu.com>
2022-01-11 16:29:13 +0800

    delete uncorrelated content

--- EIVideo/paddlevideo/modeling/heads/__init__.py
+++ EIVideo/paddlevideo/modeling/heads/__init__.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from .IntVOS import IntVOS
-from .base import BaseHead
 
-__all__ = [
-    'BaseHead'
-    , 'IntVOS'
+__all__ = ['IntVOS'
 ]
