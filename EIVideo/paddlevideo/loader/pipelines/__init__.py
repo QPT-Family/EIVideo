b127956d073f5f0a383b99d7d64760530c6c18cd
ttjygbtj <chentju@qq.com>
2022-01-17 08:12:09 +0800

    前后端连通

--- EIVideo/paddlevideo/loader/pipelines/__init__.py
+++ EIVideo/paddlevideo/loader/pipelines/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 from .manet_pipeline import Resize_manet, RandomCrop_manet, RandomHorizontalFlip_manet, ToTensor_manet, \
     RandomScale_manet
 
